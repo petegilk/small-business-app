8e87d454a6b5da73418b9910050b59f8fae1496c
peter gilkey <peter_gilkey@yahoo.com>
2020-09-16 02:04:22 -0500

    App.js boilerplate setup, components folder and Navigation created

---
+++ src/components/Navigation.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+import { Link } from 'react-router-dom'
+
+const Navigation = () => {
+  return (
+    <AppBar position="relative">
+            <Toolbar>
+                <IconButton color="inherit">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" style={{ flexGrow: "1" }}>
+                    Austin Small Business
+                </Typography>
+                <ul className="nav-list">
+
+                    <li className="nav-list-item">
+                        <Link to="/Home">Home</Link>
+                    </li>
+                    <li className="nav-list-item">
+                        <Link to="/About">About</Link>
+                    </li>
+
+                </ul>
+            </Toolbar>
+        </AppBar>
+  )
+}
+
+export default Navigation
