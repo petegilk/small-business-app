61809ac229ca09025127f19d20a0fcff47a73a8b
peter gilkey <peter_gilkey@yahoo.com>
2020-09-28 04:36:19 -0500

    Removed comments/unused code

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -38,14 +38,6 @@ const Navigation = () => {
                         <li className="nav-list-item">
                         <Link to="/login">Login</Link></li>
                     }
-                    {/* <li className="nav-list-item"
-                        onClick={() => {
-                            document.cookie = "loggedIn="
-                            window.location.replace("/login")
-                        }}>
-                        Logout
-                    </li> */}
-
                 </ul>
             </Toolbar>
         </AppBar>
